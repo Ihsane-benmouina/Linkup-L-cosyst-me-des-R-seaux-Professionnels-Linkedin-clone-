32a7f9fc8bd3ada05bcb3dac0797379386ebea58
Ihsane-12 <benminaihssane@gmail.com>
2026-06-23 16:42:45 +0100

    creation de Model Post et faire les relations entre Post et user (hasMany)

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+
+{
+    protected $fillable = ['content', 'user_id']; // Bach n-goulou l Laravel hado y9dro yt-amodifiaw
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+    //
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,11 @@ use Illuminate\Notifications\Notifiable;
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+
+public function posts()
+{
+    return $this->hasMany(Post::class);
+}
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
