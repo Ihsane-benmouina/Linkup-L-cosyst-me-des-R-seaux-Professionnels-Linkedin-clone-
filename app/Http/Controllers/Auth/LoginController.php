c199c881976bb3391eb89661cc7a989c4f77f916
Ihsane-12 <benminaihssane@gmail.com>
2026-06-29 17:05:02 +0100

    creation du fonction logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,4 +26,11 @@ class LoginController extends Controller
             'email'=>'les identifiants ne correspondent pas . '
         ])->onlyInput('email');
     }
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+        
+    }
 }
