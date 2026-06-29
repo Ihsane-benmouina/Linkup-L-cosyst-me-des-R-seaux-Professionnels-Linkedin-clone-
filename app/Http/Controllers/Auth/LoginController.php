b69a9e39f2e264377bca75b5c4b81668c71695ce
Ihsane-12 <benminaihssane@gmail.com>
2026-06-29 16:50:20 +0100

    creation du fonction login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function show(){
+        return view('auth.login');
+
+    }
+
+    public function login (Request $request){
+        $credentials = $request->validate([
+            'email'=>['required', 'email'],
+            'password'=>['required'],
+        ]);
+        if(Auth::attempt($credentials, $request->boolean('remember'))){
+            $request->session()->regenerate();
+            return redirect()->intended(route('feed'));
+        }
+        return back()->withErrors([
+            'email'=>'les identifiants ne correspondent pas . '
+        ])->onlyInput('email');
+    }
 }
