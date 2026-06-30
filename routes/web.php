24f3dd21d77b682f94ce8fa27e5cd38b06fc6f1a
Ihsane-12 <benminaihssane@gmail.com>
2026-06-30 15:23:14 +0100

    creation du controller AuthController

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/feed',[PostController::class, 'index'])->name('feed');
+
+
+
+Route::get('/register',[])->name('show.registrer'); 
+Route::get('/login',[])->name('show.login');
+
