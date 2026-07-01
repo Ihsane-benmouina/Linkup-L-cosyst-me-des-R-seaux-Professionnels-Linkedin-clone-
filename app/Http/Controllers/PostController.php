cae9137c4a73ef682ad912e4ead0cc306f57d385
Ihsane-12 <benminaihssane@gmail.com>
2026-07-01 15:29:17 +0100

    creation du fonction store pour supprimer  un poste

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
@@ -25,6 +26,13 @@ class PostController extends Controller
         ]);
         return redirect()->route('feed');
     }
+    public function formPost(){
+        return view('posts.createPost');
+    }
+    public function pageUpdate(Post $post){
+        return View('post.updatepoast', compact('post'));
+
+    }
      public function updatePost(Request $request, Post $post)
     {
         $this->authorize('update', $post);
@@ -33,6 +41,12 @@ class PostController extends Controller
             'content' => $request->content
         ]);
         return redirect()->route('feed')->with('success','post updeted seccessfully');
+    }
+    public function deletePost(Post $post){
+        $this->authorize('delete',$post);
+        $post->delete();
+        return redirect()->route('feed')->with('success','post deleted seccessfully');
+
     }
     //
 }
