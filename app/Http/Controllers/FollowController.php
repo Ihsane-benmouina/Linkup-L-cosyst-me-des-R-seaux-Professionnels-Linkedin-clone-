33a6a93eb8c62d1da17ebd5f3fd8c24025f5a363
Ihsane-12 <benminaihssane@gmail.com>
2026-07-10 11:25:08 +0100

    creation de controller FollowController

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+use Illuminate\Http\Request;
+
+class FollowController extends Controller
+{
+    //
+    public function toggle(User $user)
+    {
+        $me = Auth::user();
+
+        if ($me->id === $user->id) {
+            return back()->with('error', "Vous cannot follow yourself.");
+        }
+
+        $me->following()->toggle($user->id);
+
+        return back();
+    }
+}
